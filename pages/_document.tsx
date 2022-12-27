8992100b3e0a22ba5703cb97ce49befd39994d0d
Junya Kono <junya03dance@gmail.com>
2022-12-28 03:31:09 +0900

    fix: lint setting

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,9 +4,9 @@ import Document, {Head, Html, Main, NextScript} from 'next/document';
 const getInitialProps = createGetInitialProps();
 
 export default class extends Document {
-  static getInitialProps = getInitialProps;
+  public static getInitialProps = getInitialProps;
 
-  render() {
+  public render() {
     return (
       <Html>
         <Head />
